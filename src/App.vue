c3f1556e2d3f854c03cde78cec0d1377dc50d43d
Amos Wong <amoswwj@gmail.com>
2017-07-09 23:38:05 +0800

    Setup: Add aframe sample code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <h1>Hello world</h1>
+    <vr-content></vr-content>
+    <page-content></page-content>
   </div>
 </template>
 
 <script>
+import VrContent from './sections/VrContent';
+import PageContent from './sections/PageContent';
+
 export default {
-  
+  components: {
+    'vr-content': VrContent,
+    'page-content': PageContent,
+  },
 }
 </script>
---
+++ src/sections/PageContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="root">Welcome</div>
+</template>
+
+<style lang="scss" scoped>
+.root {
+  pointer-events: none;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0; right: 0; bottom: 0; left: 0;
+}
+</style>
